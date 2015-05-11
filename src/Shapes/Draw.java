7d19362301d53af7e73f7d837cc8532a05beb63d
angrymedoose <VinogradovaAnna95@yandex.ru>
2015-05-11 22:01:51 +0300

    Revert "1.0"
    This reverts commit b9e9729f445e00ffd12d723ef8a0370cf247d796.

--- src/Shapes/Draw.java
+++
@@ -1,8 +0,0 @@
-package Shapes;
-
-import java.awt.Graphics;
-
-public interface Draw {
-
-	public void draw(Shape shape, Graphics g);
-}
--- src/Shapes/Laba.java
+++
@@ -1,9 +0,0 @@
-package Shapes;
-
-public class Laba {
-
-	public static void main(String[] args) {
-		Frame frame = new Frame("Laba");
-	}
-
-}
